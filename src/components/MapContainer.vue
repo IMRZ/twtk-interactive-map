7319947651e75de9a58a27f78211b373b328d040
IMRZ <im.razzak@gmail.com>
2020-01-24 22:18:02 +0100

    cleanup

--- src/components/MapContainer.vue
+++ src/components/MapContainer.vue
@@ -1,16 +1,16 @@
 <template>
-  <div style="opacity: 0;" ref="mapRef" class="map">
-    <div ref="markersRef" id="markers">
-      <MapMarkerIcon
-        v-for="region in regions"
-        :key="region.key"
-        :region="region"
-        :zoomLevel="zoomLevel"
-      />
-    </div>
-
+  <div style="opacity: 0;" ref="map" class="map">
     <div style="display: none;">
-      <svg ref="svgRef" class="leaflet-interactive transparent" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 3840 3024" version="1.1">
+      <div ref="markers">
+        <MapMarkerIcon
+          v-for="region in regions"
+          :key="region.key"
+          :region="region"
+          :zoomLevel="zoomLevel"
+        />
+      </div>
+
+      <svg ref="transparentLayer" class="leaflet-interactive transparent" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 3840 3024" version="1.1">
         <path
           v-for="region in regions"
           :key="region.key"
@@ -19,7 +19,7 @@
         />
       </svg>
 
-      <svg ref="svgRef1" class="leaflet-interactive" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 3840 3024" version="1.1">
+      <svg ref="regionsLayer" class="leaflet-interactive" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 3840 3024" version="1.1">
         <path
           v-for="region in regions"
           :key="region.key"
@@ -29,7 +29,7 @@
         />
       </svg>
 
-      <svg ref="svgRef2" class="leaflet-interactive" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 3840 3024" version="1.1">
+      <svg ref="provincesLayer" class="leaflet-interactive" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 3840 3024" version="1.1">
         <path
           v-for="region in regions"
           :key="region.key"
@@ -43,13 +43,13 @@
 </template>
 
 <script>
-import { reactive, toRefs, onMounted, watch } from "@vue/composition-api";
-import MapMarkerIcon from "@/components/MapMarkerIcon.vue";
-import { L, createIcon } from "./util";
-import { useGsap } from "@/use/gsap";
-import regions from "@/data/regions.json";
+import { reactive, toRefs, watch } from '@vue/composition-api';
+import { useLeaflet, createImageLayer, createSvgLayer, createMarkerLayer } from '@/use/leaflet';
+import { fadeIn } from '@/use/gsap';
 
-// import { useLeaflet } from '@/use/leaflet';
+import MapMarkerIcon from '@/components/MapMarkerIcon.vue';
+
+import regions from '@/data/regions.json';
 
 export default {
   components: {
@@ -59,85 +59,41 @@ export default {
     path: String
   },
   setup(props) {
-    const state = reactive({
-      mapRef: null,
-      markersRef: null,
-      svgRef: null,
-      svgRef1: null,
-      svgRef2: null,
-      zoomLevel: 'high'
+    const refs = reactive({
+      map: null,
+      markers: null,
+      transparentLayer: null,
+      regionsLayer: null,
+      provincesLayer: null
     });
 
-    let map = null;
+    const bounds = [[0, 0], [3024, 3840]];
 
-    const { gsap } = useGsap();
-    const fadeInImage = () => gsap.to(map._container, 2, { opacity: 1 });
-    watch(
-      () => state.zoomLevel,
-      () => gsap.fromTo(map._panes.markerPane, { opacity: 0 }, { opacity: 1, duration: 0.5 }),
-      { lazy: true }
-    );
-
-    // useLeaflet();
-
-    onMounted(() => {
-      const bounds = [[0,0], [3024,3840]];
-
-      const imageLayer = L.imageOverlay(props.path, bounds);
-      const transparentLayer = L.svgOverlay(state.svgRef, bounds);
-      const regionsLayer = L.svgOverlay(state.svgRef1, bounds);
-      const provincesLayer = L.svgOverlay(state.svgRef2, bounds);
-
-      imageLayer.on('load', fadeInImage);
-
-      map = L.map(state.mapRef, {
-        crs: L.CRS.Simple,
-        minZoom: -2,
-        maxZoom: 2,
-        attributionControl: false,
-        maxBounds: bounds,
-        layers: [
-          imageLayer,
-          transparentLayer
-        ]
-      });
-
-      const reg = Object.values(regions);
-      const layer = [];
-      state.markersRef.children.forEach((element, i) => {
-        const icon = createIcon({ element });
-        const { name, settlement } = reg[i];
-        const m = L.marker(L.latLng(settlement.y, settlement.x), { icon }).bindPopup(name);
-        layer.push(m);
-      })
-
-      const layerGroup = L.layerGroup(layer);
-
-      map.addLayer(layerGroup);
-
-      L.control.layers({
-        'None': transparentLayer,
-        'Regions': regionsLayer,
-        'Provinces': provincesLayer,
-      }, {
-        'Resources': layerGroup
-      }).addTo(map);
-
-      map.fitBounds(bounds);
-
-      map.on('zoomend', (e) => {
-        if (e.target._zoom < -1) {
-          state.zoomLevel = "high";
-        } else if (e.target._zoom > 0) {
-          state.zoomLevel = "low";
-        } else {
-          state.zoomLevel = "med";
-        }
-      });
+    const { getMap, zoomLevel } = useLeaflet({
+      bounds: bounds,
+      refs: refs,
+      initImageLayer: () => {
+        const imageLayer = createImageLayer(props.path, bounds);
+        imageLayer.on('load', () => fadeIn(refs.map, 2)); // animate mapContainer on load
+        return imageLayer;
+      },
+      initLayers: () => ({
+        'None': createSvgLayer(refs.transparentLayer, bounds),
+        'Regions': createSvgLayer(refs.regionsLayer, bounds),
+        'Provinces': createSvgLayer(refs.provincesLayer, bounds)
+      }),
+      initOverlays: () => ({
+        'Resources': createMarkerLayer(regions, refs.markers.children, (data) => data.settlement)
+      }),
+      onReady() {
+        watch( zoomLevel, () => fadeIn(getMap()._panes.markerPane, 0.5), { lazy: true }); // animate markers on zoom
+      }
     });
 
     return {
-      ...toRefs(state),
+      ...toRefs(refs),
+      zoomLevel,
+
       regions
     };
   }
--- src/use/leaflet.js
+++ src/use/leaflet.js
@@ -1,8 +1,96 @@
-import { ref, onMounted } from "@vue/composition-api";
-import L from 'leaflet';
-import "leaflet/dist/leaflet.css";
+import Leaflet from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import { reactive, toRefs, onMounted, onUnmounted } from '@vue/composition-api';
 
-const VueIcon = L.DivIcon.extend({
+export function useLeaflet(settings) {
+  let map = null;
+
+  const state = reactive({
+    zoomLevel: 'high'
+  });
+
+  onMounted(() => {
+    const bounds = settings.bounds;
+
+    // layers
+    const imageLayer = settings.initImageLayer();
+    const layers = settings.initLayers();
+    const [firstLayer] = Object.values(layers);
+
+    // map
+    map = Leaflet.map(settings.refs.map, {
+      crs: Leaflet.CRS.Simple,
+      minZoom: -2,
+      maxZoom: 2,
+      attributionControl: false,
+      doubleClickZoom: false,
+      maxBounds: bounds,
+      layers: [
+        imageLayer,
+        firstLayer
+      ]
+    });
+
+    // overlays
+    const overlays = settings.initOverlays();
+    Object.values(overlays).forEach(overlay => map.addLayer(overlay));
+
+    // control
+    Leaflet.control.layers(layers, overlays).addTo(map);
+
+    // listeners
+    map.on('zoomend', (e) => {
+      if (e.target._zoom < -1) {
+        state.zoomLevel = 'high';
+      } else if (e.target._zoom > 0) {
+        state.zoomLevel = 'low';
+      } else {
+        state.zoomLevel = 'med';
+      }
+    });
+
+    // trigger onReady callback
+    settings.onReady && settings.onReady();
+
+    map.fitBounds(bounds);
+  });
+
+  onUnmounted(() => {
+    map.remove();
+  });
+
+  const getMap = () => map;
+
+  return {
+    getMap,
+    ...toRefs(state)
+  };
+}
+
+export function createImageLayer(imagePath, bounds) {
+  return Leaflet.imageOverlay(imagePath, bounds)
+}
+
+export function createSvgLayer(target, bounds) {
+  return Leaflet.svgOverlay(target, bounds)
+}
+
+export function createMarkerLayer(data, elements, getCoords) {
+  const markers = [];
+
+  elements.forEach((element, i) => {
+    const icon = createMarkerIcon({ element });
+    const dataItem = data[i];
+    const { y, x } = getCoords(dataItem);
+    const latLng = Leaflet.latLng(y, x);
+    const marker = Leaflet.marker(latLng, { icon }); // .bindPopup(name);
+    markers.push(marker);
+  });
+
+  return Leaflet.layerGroup(markers);
+}
+
+const VueMarkerIcon = Leaflet.DivIcon.extend({
   options: {
     element: null
   },
@@ -11,30 +99,6 @@ const VueIcon = L.DivIcon.extend({
   }
 });
 
-export function useLeaflet(settings) {
-  const mapInstance = ref(null);
-
-  onMounted(() => {
-    const bounds = [[0, 0], [settings.height, settings.width]];
-
-    const map = L.map(settings.target, {
-      crs: L.CRS.Simple,
-      minZoom: -2,
-      maxZoom: 2,
-      attributionControl: false,
-      maxBounds: bounds
-    });
-
-    /* eslint-disable-next-line */
-    const imageLayer = L.imageOverlay(settings.mapImagePath, bounds).addTo(map);
-    // imageLayer.on('load', fadeInImage);
-  });
-
-  return {
-    mapInstance
-  };
-}
-
-export function createIcon(options) {
-  return new VueIcon(options);
+function createMarkerIcon(options) {
+  return new VueMarkerIcon(options);
 }
