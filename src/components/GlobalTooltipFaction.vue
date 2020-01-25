364e9cd03bafc75afa0ac4e97324a9e8f2bb0420
IMRZ <IMRZ@users.noreply.github.com>
2020-01-25 15:06:44 +0100

    fixed faction tooltip for painter. (#3)

---
+++ src/components/GlobalTooltipFaction.vue
@@ -0,0 +1,35 @@
+<template>
+  <pre class="faction">{{factionName}}</pre>
+</template>
+
+<script>
+import { computed } from '@vue/composition-api';
+
+import factions from '@/data/common/factions';
+
+export default {
+  props: {
+    tooltip: Object
+  },
+  setup(props) {
+    const factionName = computed(() => {
+      const factionKey = props.tooltip.value;
+      if (factionKey) {
+        return factions[factionKey].name;
+      } else {
+        return 'Abanoned';
+      }
+    });
+
+    return {
+      factionName
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.faction {
+  margin: 10px;
+}
+</style>
