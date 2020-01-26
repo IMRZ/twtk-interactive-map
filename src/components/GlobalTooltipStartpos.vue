554c0042ad4f4f8fd6bcab999afccc23610b3186
IMRZ <im.razzak@gmail.com>
2020-01-26 21:38:22 +0100

    only show faction name on  startpos tooltip.

--- src/components/GlobalTooltipStartpos.vue
+++ src/components/GlobalTooltipStartpos.vue
@@ -1,20 +1,13 @@
 <template>
   <div class="startpos">
-    <div>
-      <span class="label">Name:</span>
-      <span>{{tooltip.faction.adjective}}</span>
-    </div>
-    <div>
-      <span class="label">Faction:</span>
-      <span>{{tooltip.faction.name}}</span>
-    </div>
+    <span>{{tooltip.faction.adjective}}</span>
   </div>
 </template>
 
 <script>
 export default {
   props: {
-    tooltip: [Object]
+    tooltip: Object
   }
 };
 </script>
@@ -23,10 +16,5 @@ export default {
 .startpos {
   font-family: monospace;
   margin: 10px;
-
-  .label {
-    display: inline-block;
-    min-width: 70px;
-  }
 }
 </style>
